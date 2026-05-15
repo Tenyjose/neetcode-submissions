7fd3c41fe6280da5ba054307b03c35887ee67a47
Teny Jose <49028486+Tenyjose@users.noreply.github.com>
2026-05-15 21:21:15 +0100

    Add: pacific-atlantic-water-flow - submission-9

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-9.py
@@ -0,0 +1,46 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        pacific, atlantic = set(), set()
+
+
+        # water flows from all cells in left column and top row to the pacific 
+        # and water flows all bottom rows and right column to the atlantic
+        # so a better approach would be to move from the ocean and perfom a dfs
+        # So when we start from an ocean border, we can move to a neighbour only 
+        # if the neighbour height is greater than or equal to the current height.
+
+        def dfs(r,c,visited,prevHeight):
+            if (r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                (r,c) in visited or
+                heights[r][c] < prevHeight):
+                return
+
+            visited.add((r,c))
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                dfs(nr, nc, visited, heights[r][c])
+
+
+        for c in range(cols):
+            dfs(0,c,pacific,heights[0][c])
+            dfs(rows-1,c,atlantic,heights[rows-1][c])
+
+        for r in range(rows):
+            dfs(r,0,pacific,heights[r][0])
+            dfs(r,cols-1,atlantic,heights[r][cols-1])
+
+
+        result = []
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in atlantic and (r,c) in pacific:
+                    result.append([r,c])
+
+        return result
+
+        
