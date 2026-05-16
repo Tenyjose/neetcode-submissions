2088b4de58316c01fc7cb874b31bf5c1f0cf4bbf
Teny Jose <49028486+Tenyjose@users.noreply.github.com>
2026-05-16 14:40:26 +0100

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        indegree = [0]*numCourses
+        result = []
+        # first we have to create an adjacency list
+        graph = {i : [] for i in range(numCourses)}
+        for c,p in prerequisites:
+            graph[p].append(c)  # notice we are geeting for each prerequisite.
+            indegree[c] += 1
+
+        queue = deque()
+        for num in range (numCourses):
+            if indegree[num] == 0:
+                queue.append(num)
+
+        while queue:
+            course = queue.popleft()
+            result.append(course)
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+                if  indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        if len(result) == numCourses:
+            return result
+        else:
+            return []
